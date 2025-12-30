56ca263883c0c7ce57c61aa5607cdf28f828ca05
pazyork <pazyorkcc@gmail.com>
2025-12-30 12:01:58 +0800

    softmax进阶实现

--- notebook/utiltool.py
+++ notebook/utiltool.py
@@ -5,13 +5,10 @@ class UtilTool(nn.Module):
     
     @classmethod
     def softmax(cls, in_features:torch.Tensor,dim:int):
-        in_dtype=in_features.dtype
-        in_features=in_features.to(torch.float64)
-        print('in_features',in_features)
-        ## x,y,z
-        features_base=torch.exp(in_features)
-        print('features_base',features_base)
         ## x,1,z
+        in_features_max=in_features.max(dim=dim,keepdim=True).values
+        ## x,y,z
+        features_base=torch.exp(in_features-in_features_max)
+        ## x,y,z
         features_sum=features_base.sum(dim=dim,keepdim=True)
-        print('features_sum',features_sum)
-        return (features_base/features_sum).to(in_dtype)
+        return (features_base/features_sum)
